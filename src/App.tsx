80f0d967eb9b6ed7dccbb908740514300bd2959a
jacky1a2a3a4a <jacky1a2a3a4a@gmail.com>
2025-03-11 22:38:02 +0800

    feature- project-start

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import GlobalStyles from "./styles/GlobalStyles";
+
+
+function App() {
+  return (
+    <div>
+      <GlobalStyles />
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
